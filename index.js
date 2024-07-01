870573ea3afe6f79b39ffa10ef016f745065cb7b
zvbt <cyadine@pm.me>
2024-07-02 00:58:38 +0200

    Prettier formatter

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const port = 3038;
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+    if (req.url === "/") {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        const filePath = path.join(__dirname, 'generator.html');
-        fs.readFile(filePath, 'utf8', (err, data) => {
+        res.setHeader("Content-Type", "text/html");
+        const filePath = path.join(__dirname, "generator.html");
+        fs.readFile(filePath, "utf8", (err, data) => {
             if (err) {
                 res.statusCode = 500;
-                res.end('<h1>Failed to read file</h1>');
+                res.end("<h1>Failed to read file</h1>");
                 return;
             }
             res.end(data);
         });
-    } else if (req.url === '/collection') {
+    } else if (req.url === "/collection") {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        const filePath = path.join(__dirname, '/data/hydra.json');
-        fs.readFile(filePath, 'utf8', (err, data) => {
+        res.setHeader("Content-Type", "application/json");
+        const filePath = path.join(__dirname, "/data/hydra.json");
+        fs.readFile(filePath, "utf8", (err, data) => {
             if (err) {
                 res.statusCode = 500;
-                res.end(JSON.stringify({ error: 'Failed to read file' }));
+                res.end(JSON.stringify({ error: "Failed to read file" }));
                 return;
             }
             res.end(data);
         });
-    } else if (req.url === '/adult') {
+    } else if (req.url === "/adult") {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        const filePath = path.join(__dirname, '/data/adult.json');
-        fs.readFile(filePath, 'utf8', (err, data) => {
+        res.setHeader("Content-Type", "application/json");
+        const filePath = path.join(__dirname, "/data/adult.json");
+        fs.readFile(filePath, "utf8", (err, data) => {
             if (err) {
                 res.statusCode = 500;
-                res.end(JSON.stringify({ error: 'Failed to read file' }));
+                res.end(JSON.stringify({ error: "Failed to read file" }));
                 return;
             }
             res.end(data);
         });
     } else {
         res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('404 Not Found');
+        res.setHeader("Content-Type", "text/plain");
+        res.end("404 Not Found");
     }
 });
 
